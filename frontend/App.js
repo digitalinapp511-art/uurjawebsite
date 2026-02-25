c606d33e9f52cd92535c125d1da6e892fe88e2e6
Love Saini <sainilove910@gmail.com>
2026-02-25 19:10:34 +0530

    fixed Images

--- frontend/App.js
+++ frontend/App.js
@@ -10,17 +10,17 @@ const App = () => {
     cloud: { cloudName: 'dqkssrvir' }
   });
 
-  const imageIds = [
-    "bracelete6_awqjal",
-    "bracelete9_x8gd2o",
-    "bracelete2_bfnbrh",
-    "bracelete8_l35rg8",
-    "bracelete1_kdxnja",
-    "bracelete3_dpcjmu",
-    "bracelete5_pudumv",
-    "bracelete7_go7dxq",
-    "bracelete10_uvjnha"
-  ];
+  // const imageIds = [
+  //   "bracelete6_awqjal",
+  //   "bracelete9_x8gd2o",
+  //   "bracelete2_bfnbrh",
+  //   "bracelete8_l35rg8",
+  //   "bracelete1_kdxnja",
+  //   "bracelete3_dpcjmu",
+  //   "bracelete5_pudumv",
+  //   "bracelete7_go7dxq",
+  //   "bracelete10_uvjnha"
+  // ];
 
   return (
     <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
