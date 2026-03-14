e2ae99154a434630bb9bb830bb5a42859f730468
Love Saini <sainilove910@gmail.com>
2026-03-14 17:47:22 +0530

    fixed ENV

--- backend/routes/firebase.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { firebaseLogin } from "../controllers/firebaseLogin.js";
-
-const router = express.Router();
-
-router.post("/login", firebaseLogin);
-
-export default router;
